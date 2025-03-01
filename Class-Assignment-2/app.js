45f974b3467c58de3dc53e1a9661c673134c192e
Abdullah <162957435+AbdullahNisar123@users.noreply.github.com>
2025-03-01 20:10:25 +0500

    class 2 assingment  done

---
+++ Class-Assignment-2/app.js
@@ -0,0 +1,61 @@
+// let imgEle = document.querySelectorAll("img")[0]
+// let bodyEle = document.querySelectorAll("body")[0]
+// let offBulb = "https://www.citypng.com/public/uploads/preview/transparent-hd-real-light-bulb-704081695125968fldw2x56hs.png"
+// let onBulb = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR25C0t7Ak4B5qWwT4nUMqlbGqF51wUK6xMyw&s"
+// function btnOn() {
+//     if(imgEle.src === offBulb){
+//         imgEle.src = onBulb
+//         bodyEle.style.backgroundColor = "white"
+//     }
+// }
+// function btnOff(){
+//     if(imgEle.src === onBulb){
+//         imgEle.src = offBulb
+//         bodyEle.style.backgroundColor = "black"
+//     }
+// }
+
+
+
+
+
+
+
+// let h1 = document.querySelectorAll(".captain")[0]
+// let captains = ["Rizwan", "babar","Sarfaraz","Lala"]
+
+// function changeCaptain(){
+//     let currentCap = h1.innerHTML;
+//     let currentCapIdx = captains.indexOf(currentCap)
+//     let nextCapIdx = currentCapIdx + 1;
+//     if(nextCapIdx === captains.length){
+//         nextCapIdx = 0
+//     }
+//     h1.innerHTML = captains[nextCapIdx]
+// }
+
+
+
+
+
+
+
+// let head = "http://127.0.0.1:5500/images/head.png"
+// let tail = "http://127.0.0.1:5500/images/tail.png"
+
+// let img = document.querySelectorAll("img")[0]
+// function changeSide(){
+//     if(img.src == head){
+//         img.src = tail
+//     }
+//     else if(img.src === tail){
+//         img.src = head
+//     }
+ 
+// }
+
+
+
+
+
+
