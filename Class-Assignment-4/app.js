1a2b795cee4a5bee1decabd652145965825bd4a5
Abdullah <162957435+AbdullahNisar123@users.noreply.github.com>
2025-03-05 17:25:40 +0500

    feat: where allow user to select option

--- Class-Assignment-4/app.js
+++ Class-Assignment-4/app.js
@@ -3,48 +3,54 @@ let TeamB = document.querySelectorAll(".teamBImg")[0];
 let TeamAScore = document.querySelectorAll(".teamAScore")[0];
 let TeamBScore = document.querySelectorAll(".teamBScore")[0];
 let winnerName = document.querySelectorAll(".winnerName")[0];
-let rock = "images/rock.png";
-let paper = "images/paper.png";
-let scissors = "images/scissors.png";
+let rockImg = "images/rock.png";
+let paperImg = "images/paper.png";
+let scissorsImg = "images/scissors.png";
+let paper = "paper"
+let rock = "rock"
+let scissor = "scissor"
 
-function play() {
-    let num1 = Math.ceil(Math.random() * 3);
+
+function play(user) {
+    // let num1 = Math.ceil(Math.random() * 3);
+    let num1 = user;
     let num2 = Math.ceil(Math.random() * 3);
-
+    // console.log(num1)
+    // console.log(num2)
     // select items
-    if (num1 === 1) {
-        TeamA.src = rock
+    if (num1 === rock) {
+        TeamA.src = rockImg
     } 
-    else if (num1 === 2) {
-        TeamA.src = paper
+    else if (num1 === paper) {
+        TeamA.src = paperImg
     }
-    else if (num1 === 3) {
-        TeamA.src = scissors
+    else if (num1 === scissor) {
+        TeamA.src = scissorsImg
     }
 
 
     // add img according to the item
     if (num2 === 1) {
-        TeamB.src = rock
+        TeamB.src = rockImg
     } 
     else if (num2 === 2) {
-        TeamB.src = paper
+        TeamB.src = paperImg
     }
     else if (num2 === 3) {
-        TeamB.src = scissors
+        TeamB.src = scissorsImg
     }
 
     // check the winner and update score
-    if (num1 === 1 && num2 === 3 || num1 === 2 && num2 === 1 || num1 === 3 && num2 === 2) {
+    if (num1 === rock && num2 === 3 || num1 === paper && num2 === 1 || num1 === scissor && num2 === 2) {
         TeamAScore.innerHTML = Number(TeamAScore.innerHTML) + 1;
         winnerName.innerHTML = "Team A wins";
 
     }
-    if (num2 === 1 && num1 === 3 || num2 === 2 && num1 === 1 || num2 === 3 && num1 === 2) {
+    if (num2 === 1 && num1 === scissor || num2 === 2 && num1 === rock || num2 === 3 && num1 === paper) {
         TeamBScore.innerHTML = Number(TeamBScore.innerHTML) + 1;
         winnerName.innerHTML = "Team B wins";
     }
-    else if (num1 === 1 && num2 === 1 || num1 === 2 && num2 === 2 || num1 === 3 && num2 === 3) {
+    else if (num1 === rock && num2 === 1 || num1 === paper && num2 === 2 || num1 === scissor && num2 === 3) {
         winnerName.innerHTML = "It's a draw";
     }
 }
